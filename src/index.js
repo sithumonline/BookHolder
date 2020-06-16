21f5564a18e60599c95a339f7c6035d534f1c02b
ebonynon <sithumsandeepap@gmail.com>
2020-06-16 18:41:44 +0530

    add theme

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
@@ -9,17 +10,24 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 import MainPage from "views/MainPage/MainPage.js";
 import CreateBookPage from "views/CreateBookPage/CreateBookPage.js";
 import ShowBookDetailsPage from "views/ShowBookDetailsPage/ShowBookDetails.js";
-// import UpdateBookInfo from "./components/UpdateBookInfo";
 
 var hist = createBrowserHistory();
 
+const muiTheme = createMuiTheme({
+  palette: {
+    type: "light" //"dark",
+  },
+});
+
 ReactDOM.render(
   <Router history={hist}>
-    <Switch>
-      <Route path="/show-book/" component={ShowBookDetailsPage} />
-      <Route path="/create-book" component={CreateBookPage} />
-      <Route path="/" component={MainPage} />
-    </Switch>
+    <MuiThemeProvider theme={muiTheme}>
+      <Switch>
+        <Route path="/show-book/" component={ShowBookDetailsPage} />
+        <Route path="/create-book" component={CreateBookPage} />
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </MuiThemeProvider>
   </Router>,
   document.getElementById("root")
 );
