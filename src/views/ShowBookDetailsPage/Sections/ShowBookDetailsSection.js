37cb8b4d22490ec0feb0f6c4cb04a922a7041cc6
ebonynon <sithumsandeepap@gmail.com>
2020-06-16 18:24:35 +0530

    show dook det :1

--- src/views/ShowBookDetailsPage/Sections/ShowBookDetailsSection.js
+++ src/views/ShowBookDetailsPage/Sections/ShowBookDetailsSection.js
@@ -11,8 +11,8 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 // core components
 import GridContainer from "../../../components/Grid/GridContainer.js";
 import GridItem from "../../../components/Grid/GridItem.js";
@@ -129,90 +129,3 @@ export default class SectionShowBookDetails extends Component {
     return <Body />;
   }
 }
-
-// let BookItem = <div>
-// <table className="table table-hover table-dark">
-//   {/* <thead>
-//     <tr>
-//       <th scope="col">#</th>
-//       <th scope="col">First</th>
-//       <th scope="col">Last</th>
-//       <th scope="col">Handle</th>
-//     </tr>
-//   </thead> */}
-//   <tbody>
-//     <tr>
-//       <th scope="row">1</th>
-//       <td>Title</td>
-//       <td>{ book.title }</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">2</th>
-//       <td>Author</td>
-//       <td>{ book.author }</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">3</th>
-//       <td>ISBN</td>
-//       <td>{ book.isbn }</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">4</th>
-//       <td>Publisher</td>
-//       <td>{ book.publisher }</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">5</th>
-//       <td>Published Date</td>
-//       <td>{ book.published_date }</td>
-//     </tr>
-//     <tr>
-//       <th scope="row">6</th>
-//       <td>Description</td>
-//       <td>{ book.description }</td>
-//     </tr>
-//   </tbody>
-// </table>
-// </div>
-// ........................
-// <div className="ShowBookDetails">
-//             <div className="container">
-//               <div className="row">
-//                 <div className="col-md-10 m-auto">
-//                   <br /> <br />
-//                   <Link to="/" className="btn btn-outline-warning float-left">
-//                       Show Book List
-//                   </Link>
-//                 </div>
-//                 <br />
-//                 <div className="col-md-8 m-auto">
-//                   <h1 className="display-4 text-center">Book's Record</h1>
-//                   <p className="lead text-center">
-//                       View Book's Info
-//                   </p>
-//                   <hr /> <br />
-//                 </div>
-//               </div>
-//               <div>
-//                 { BookItem }
-//               </div>
-
-//               <div className="row">
-//                 <div className="col-md-6">
-//                   <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={this.onDeleteClick.bind(this,book._id)}>Delete Book</button><br />
-//                 </div>
-
-//                 <div className="col-md-6">
-//                   <Link to={`/edit-book/${book._id}`} className="btn btn-outline-info btn-lg btn-block">
-//                         Edit Book
-//                   </Link>
-//                   <br />
-//                 </div>
-
-//               </div>
-//                 {/* <br />
-//                 <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Book</button>
-//                 <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Book</button> */}
-
-//             </div>
-//           </div>
