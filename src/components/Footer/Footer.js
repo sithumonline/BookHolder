c8d3042adc2fded8515af1922fa070a3ccdbafe9
ebonynon <sithumsandeepap@gmail.com>
2020-06-16 18:35:12 +0530

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -30,10 +30,10 @@ export default function Footer(props) {
     <footer className={footerClasses}>
       <div className={classes.container}>
         <div className={classes.center}>
-          <a href="https://github.com/ebonynon/KallathoniXY">
+          <a href="https://github.com/ebonynon/BookHolder">
             <img
               alt="ebonynon/KallathoniXY Build"
-              src="https://github.com/ebonynon/KallathoniXY/workflows/Build/badge.svg?branch=master&event=push"
+              src="https://github.com/ebonynon/BookHolder/workflows/Build/badge.svg?branch=master&event=push"
             />
           </a>
           &nbsp;&copy; {1900 + new Date().getYear()} , made with{" "}
